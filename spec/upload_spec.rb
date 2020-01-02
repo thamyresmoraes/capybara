72472b6facb98ea6c30d994268419320a24a4526
thamyresmoraes <thamyresmoraes@live.com>
2020-01-02 16:47:55 -0200

    upload de arquivos

---
+++ spec/upload_spec.rb
@@ -0,0 +1,31 @@
+describe 'Upload', :upload do
+
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/upload'
+        @arquivo = Dir.pwd + '/spec/fixtures/texto.txt'
+        @imagem = Dir.pwd + '/spec/fixtures/imagem.jpg'
+
+    end
+
+    it 'upload com arquivo texto' do
+        attach_file('file-upload', @arquivo)
+        click_button 'Upload'
+
+        div_arquivo = find('#uploaded-file')
+        expect(div_arquivo.text).to eql 'texto.txt'
+    end
+
+    it 'upload com arquivo imagem' do
+        attach_file('file-upload', @imagem)
+        click_button 'Upload'
+
+        sleep 5
+        img = find('#new-image')
+        expect(img[:src]).to include '/uploads/imagem.jpg'
+    end
+
+    after(:each) do
+        sleep 3
+    end
+
+end
